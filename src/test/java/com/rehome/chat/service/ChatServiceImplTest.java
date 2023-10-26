925ac7b79cc0406f0b5b2c771e525e20e9958f8a
yongchanglim <100824874+yongchanglim@users.noreply.github.com>
2023-10-26 22:09:23 +0800

    add test

---
+++ src/test/java/com/rehome/chat/service/ChatServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.rehome.chat.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ChatServiceImplTest {
+  @Test
+  void contextLoads() {
+    assertEquals(1, 1);
+  }
+}
